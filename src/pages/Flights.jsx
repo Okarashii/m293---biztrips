8ae891601c16f8dada325ca25645afdef93adc71
Christian Nuttli <nuttli.christian@gmail.com>
2024-06-27 09:08:42 +0200

    - minor additions

--- src/pages/Flights.jsx
+++ src/pages/Flights.jsx
@@ -1,7 +1,13 @@
 import { useState, useEffect } from "react"
+import { getFlights } from "../services/flightServices";
 
 export default function Flights() {
-	
+	const [airports, setAirports] = useState([]);
+
+	useEffect(() => {
+		getFlights().then(r => setAirports(r))
+	}, [])
+
 	return (
 		<form className="grid grid-cols-2 w-fit">
 			<label htmlFor="fromid">Abflugsort</label>
