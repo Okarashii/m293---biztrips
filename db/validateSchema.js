dbdbe14360b7a3aefd62a020b22fe30b3ab5cf44
Fujitaka Okarashii <nuttli.christian@gmail.com>
2024-07-06 21:33:57 +0200

    refactoring for optimization

--- db/validateSchema.js
+++ db/validateSchema.js
@@ -3,36 +3,206 @@ const path = require('path');
 
 const dbPath = path.resolve(__dirname, '../db.json');
 
-const meetingSchema = {
-	tripID: 0,
-	hotelID: 0,
-	meetingRoomNr: "",
-	date: {
-		year: 0,
-		month: 0,
-		day: 0
+const airportSchema = {
+	required: {
+		iataCode: "",
+		name: "",
+		country: "",
+		city: ""
 	},
-	startTime: {
-		hour: -1,
-		minute: -1
+	optional: {}
+};
+
+const flightSchema = {
+	required: {
+		departureAirportIATA: "",
+		destinationAirportIATA: "",
+		airline: "",
+		economyPrice: 0,
+		premiumEconomyPrice: 0,
+		businessPrice: 0,
+		firstClassPrice: 0
 	},
-	endTime: {
-		hour: -1,
-		minute: -1
+	optional: {}
+};
+
+const hotelSchema = {
+	required: {
+		nearAirportIATA: "",
+		name: "",
+		address: "",
+		city: "",
+		contactFirstname: "",
+		contactlastname: "",
+		contactEmail: "",
+	},
+	optional: {
+		img: "",
 	}
+};
+
+const roomSchema = {
+	required: {
+		roomNr: "",
+		hotelID: 0,
+		capacity: 0,
+		price: 0,
+		isMeetingRoom: false
+	},
+	optional: {}
+};
+
+const meetingSchema = {
+	required: {
+		tripID: 0,
+		hotelID: 0,
+		meetingRoomNr: "",
+		date: {
+			required: {
+				year: 0,
+				month: 0,
+				day: 0
+			},
+			optional: {}
+		},
+		startTime: {
+			required: {
+				hour: 0,
+				minute: 0
+			},
+			optional: {}
+		},
+		endTime: {
+			required: {
+				hour: 0,
+				minute: 0
+			},
+			optional: {}
+		}
+	},
+	optional: {}
+};
+
+const participantSchema = {
+	required: {
+		tripID: 0,
+		firstname: "",
+		lastname: "",
+		address: "",
+		city: "",
+		email: ""
+	},
+	optional: {}
+};
+
+const planeTicketSchema = {
+	required: {
+		flightID: 0,
+		participantID: 0,
+		class: "",
+		price: 0,
+		departureDateTime: {
+			required: {
+				year: 0,
+				month: 0,
+				day: 0,
+				hour: 0,
+				minute: 0
+			},
+			optional: {}
+		},
+		arrivalDateTime: {
+			required: {
+				year: 0,
+				month: 0,
+				day: 0,
+				hour: 0,
+				minute: 0
+			},
+			optional: {}
+		}
+	},
+	optional: {}
+};
+
+const hotelBookingSchema = {
+	required: {
+		hotelID: 0,
+		participantID: 0,
+		roomNr: "",
+		price: 0,
+		checkinDateTime: {
+			required: {
+				year: 0,
+				month: 0,
+				day: 0,
+				hour: 0,
+				minute: 0
+			},
+			optional: {}
+		},
+		checkoutDateTime: {
+			required: {
+				year: 0,
+				month: 0,
+				day: 0,
+				hour: 0,
+				minute: 0
+			},
+			optional: {}
+		}
+	},
+	optional: {}
 }
 
-const checkInteger = (integer, min=0, max=Infinity) => Number.parseInt(integer) === integer && integer >= min && integer <= max;
-const checkFloat = (float) => !Number.isNaN(Number.parseFloat(float))
-const checkString = (string) => string !== undefined && string !== "";
-const checkIATACode = (iataCode) => checkString(iataCode) && iataCode.length === 3 && iataCode.toUppercase() === iataCode;
-const checkDate = ({year, month, day}) => checkInteger(year, new Date().getFullYear()) && checkInteger(month, 1, 12) && checkInteger(day, 1, 31);
-const checkTime = ({hour, minute}) => checkInteger(hour, 0, 23) && checkInteger(minute, 0, 59);
+const tripSchema = {
+	required: {
+		startDate: {
+			required: {
+				year: 0,
+				month: 0,
+				day: 0
+			},
+			optional: {}
+		},
+		endDate: {
+			required: {
+				year: 0,
+				month: 0,
+				day: 0
+			},
+			optional: {}
+		}
+	},
+	optional: {}
+}
+
+function hasValidProps(schema, obj) {
+	return hasSameProperties(schema, obj)
+}
+
+function hasSameProperties({ required, optional }, obj) {
+	return Object.keys(obj).every((prop) => {
+		if (typeof obj[prop] !== 'object') {
+			return (required.hasOwnProperty(prop) || optional.hasOwnProperty(prop)) && Object.keys(required).every((p) => obj.hasOwnProperty(p));
+		}
+		else {
+			return hasSameProperties(required[prop], obj[prop]) || hasSameProperties(optional[prop], obj[prop]);
+		}
+	})
+}
+
+function hasIdConflicts(resourceName, id) {
+	console.log("resourceName: ", resourceName);
+	const db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
+	return db[resourceName].find(item => item.id == id);
+}
 
 module.exports = (req, res, next) => {
-	if (["POST", "PUT", "PATCH"].includes(req.method)) {
+	const { body, method } = req;
+	if (["POST", "PUT", "PATCH"].includes(method)) {
 		const resourceName = req.path.substring(1).split('/')[0];
-		const { id } = req.body;
+		const { id } = body;
 		if (hasIdConflicts(resourceName, id)) {
 			res.status(409).jsonp();
 			return;
@@ -43,68 +213,57 @@ module.exports = (req, res, next) => {
 				res.status(400).jsonp();
 				return;
 			case "airports":
-				const { name, iataCode, country, city } = req.body;
-				if (!checkString(name) || !checkString(country) || !checkString(city) || !checkIATACode(iataCode)) {
+				if(!hasValidProps(airportSchema, body, method)) {
 					res.status(400).jsonp();
 					return;
 				}
 
-				if (req.method === "POST") {
-					const db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
-					if (db['airports'].find(item => item.iataCode === iataCode)) {
-						res.status(409).jsonp();
-						return;
-					}
-				}
-
 				break;
 			case "flights":
-				const { departureAirportIATA, destinationAirportIATA, airline, economyPrice, premiumEconomyPrice, businessPrice, firstClassPrice } = req.body;
-				if (!checkIATACode(departureAirportIATA) || !checkIATACode(destinationAirportIATA) || !checkString(airline) || !checkFloat(economyPrice) || !checkFloat(premiumEconomyPrice) || !checkFloat(businessPrice) || !checkFloat(firstClassPrice)) {
+				if(!hasValidProps(flightSchema, body, method)) {
 					res.status(400).jsonp();
 					return;
 				}
 				break;
 			case "hotels":
-				const { nearAirportIATA, name: hotelName, address, city: hotelCity, contactFirstname, contactLastname, contactEmail } = req.body;
-				if (!checkIATACode(nearAirportIATA) || !checkString(hotelName) || !checkString(hotelCity) || !checkString(address) || !checkString(contactFirstname) || !checkString(contactLastname) || !checkString(contactEmail)) {
+				if(!hasValidProps(hotelSchema, body, method)) {
 					res.status(400).jsonp();
 					return;
 				}
 
 				break;
 			case "rooms":
-				const { roomNr, hotelID, capacity, price, isMeetingRoom } = req.body;
-				const queryHotelID = req.query.hotelID;
-
-				console.log("req.body: ", req.body);
-				console.log("req.query: ", req.query);
-				if (!checkInteger(hotelID) || !checkString(roomNr) || !checkInteger(capacity) || !checkFloat(price) || isMeetingRoom === undefined || queryHotelID != hotelID) {
+				if(!hasValidProps(roomSchema, body, method)) {
 					res.status(400).jsonp();
 					return;
 				}
 				break;
 			case "meetings":
-				const { tripID:meetingTripID, hotelID: meetingHotelID, meetingRoomNr, date, startTime, endTime } = req.body;
-				console.log(req.body);
-				if(req.method === "PATCH") {
-					if (!hasSameProperties(meetingSchema, req.body)) {
-						res.status(400).jsonp();
-						return;
-					}
-				}
-				else if (!hasAllSameProperties(meetingSchema, req.body)) {
+				if(!hasValidProps(meetingSchema, body, method)) {
 					res.status(400).jsonp();
 					return;
 				}
-				// else if (req.query.tripID != meetingTripID || !checkInteger(meetingTripID) || !checkInteger(meetingHotelID) || !checkString(meetingRoomNr) || !checkDate(date) || !checkTime(startTime) || !checkTime(endTime)) {
-				// 	res.status(400).jsonp();
-				// 	return;
-				// }
 				break;
 			case "participants":
-				const { tripID: participantTripID, firstname, lastname, address: participantAddress, city: participantCity, email } = req.body;
-				if (!checkInteger(participantTripID) || !checkString(firstname) || !checkString(lastname) || !checkString(participantAddress) || !checkString(participantCity) || checkString(email) || req.query.tripID != participantTripID) {
+				if(!hasValidProps(participantSchema, body, method)) {
+					res.status(400).jsonp();
+					return;
+				}
+				break;
+			case "planeTickets":
+				if(!hasValidProps(planeTicketSchema, body, method)) {
+					res.status(400).jsonp();
+					return;
+				}
+				break;
+			case "hotelBooking":
+				if(!hasValidProps(hotelBookingSchema, body, method)) {
+					res.status(400).jsonp();
+					return;
+				}
+				break;
+			case "trips":
+				if(!hasValidProps(tripSchema, body, method)) {
 					res.status(400).jsonp();
 					return;
 				}
@@ -113,36 +272,4 @@ module.exports = (req, res, next) => {
 	}
 
 	next();
-}
-
-function hasIdConflicts(resourceName, id) {
-	console.log("resourceName: ", resourceName);
-	const db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
-	return db[resourceName].find(item => item.id == id);
-}
-
-function hasSameProperties(schema, obj) {
-	const hasSameProps = Object.keys(obj).every((prop) => {
-		if (typeof obj[prop] !== 'object') {
-			const result = schema.hasOwnProperty(prop);
-			return result;
-		}
-		else {
-			const result = hasSameProperties(schema[prop], obj[prop]);
-			return result;
-		}
-	})
-
-	return hasSameProps;
-}
-
-function hasAllSameProperties(schema, obj) {
-	return Object.keys(schema).every((prop) => {
-		if (typeof obj[prop] !== 'object') {
-			return obj.hasOwnProperty(prop);
-		}
-		else {
-			return obj.hasAllSameProperties(schema[prop], obj[prop]);
-		}
-	})
 }
